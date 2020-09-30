e2def697fa5ee5d41e66963d47b42e3c3d0517fe
MikkelRBoisen <mikkel.rugholm.boisen@post.au.dk>
2020-09-30 12:41:26 +0200

    startet på settingsactivity + tilføjet billeder til locationactivity+ tilføjet testknappe i mainactivity

--- app/src/main/java/com/example/st7bac_2020e105/Activities/MainActivity.java
+++ app/src/main/java/com/example/st7bac_2020e105/Activities/MainActivity.java
@@ -2,15 +2,30 @@ package com.example.st7bac_2020e105.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.st7bac_2020e105.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button testActivities;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        testActivities = (Button)findViewById(R.id.testActivitiesButton);
+        testActivities.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, SettingsActivity.class); //ÆNDRER ACTIVITES FOR AT TESTE!
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/st7bac_2020e105/Activities/SettingsActivity.java
+++ app/src/main/java/com/example/st7bac_2020e105/Activities/SettingsActivity.java
@@ -2,15 +2,56 @@ package com.example.st7bac_2020e105.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.os.Bundle;
+import android.widget.SeekBar;
 
 import com.example.st7bac_2020e105.R;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    //https://stackoverflow.com/questions/10134338/using-seekbar-to-control-volume-in-android
+
+    private SeekBar volume = null;
+    SeekBar radius;
+    private AudioManager audioManager = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        controlVolume();
+    }
+
+    private void controlVolume()
+    {
+        try {
+            volume = (SeekBar)findViewById(R.id.seekBar_volume_settingsactivity);
+            audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+            volume.setMax(audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+            volume.setProgress(audioManager.getStreamVolume(AudioManager.STREAM_MUSIC));
+
+            volume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                }
+            });
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
