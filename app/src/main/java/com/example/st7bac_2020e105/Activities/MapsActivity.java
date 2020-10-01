636a6db85d2a52d210c400634e93c5943b3773ed
Nicolai Baech <baech97@gmail.com>
2020-10-01 10:01:50 +0200

    Created MapsActivity

---
+++ app/src/main/java/com/example/st7bac_2020e105/Activities/MapsActivity.java
@@ -0,0 +1,16 @@
+package com.example.st7bac_2020e105.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.st7bac_2020e105.R;
+
+public class MapsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+    }
+}
