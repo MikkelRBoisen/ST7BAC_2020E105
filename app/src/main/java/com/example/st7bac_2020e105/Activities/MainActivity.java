b389eec4c59116ef9d94bfeecb8f620cf7475b81
Nicolai Baech <baech97@gmail.com>
2020-10-12 12:31:19 +0200

    Live location in maps works + image marker to show live location

--- app/src/main/java/com/example/st7bac_2020e105/Activities/MainActivity.java
+++ app/src/main/java/com/example/st7bac_2020e105/Activities/MainActivity.java
@@ -1,15 +1,38 @@
 package com.example.st7bac_2020e105.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.st7bac_2020e105.R;
 
 public class MainActivity extends AppCompatActivity {
+    private static final long MIN_TIME_BETWEEN_LOCATION_UPDATES = 1000;    // milisecs
+    private static final float MIN_DISTANCE_MOVED_BETWEEN_LOCATION_UPDATES = 1;  // meters
+    public static final String EXTRA_USER_LATITUDE = "location_latitude";
+    public static final String EXTRA_USER_LONGITUDE = "location_longitude";
+
+    public static final int PERMISSIONS_REQUEST_LOCATION = 189;
+
+    private boolean isTracking = false;
+    private LocationManager locationManager;
+    private Location userLocation;
+    private double latitude, longitude;
 
     Button test1;
     Button test2;
@@ -32,13 +55,161 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+
         test2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, LoginActivity.class));
-
+                Intent mapIntent = new Intent(getApplicationContext(), MapsActivity.class);
+                if (userLocation != null) {
+                    //if location known, send it to the map activity
+                    mapIntent.putExtra(EXTRA_USER_LATITUDE, userLocation.getLatitude());
+                    mapIntent.putExtra(EXTRA_USER_LONGITUDE, userLocation.getLongitude());
+                }
+                startActivity(mapIntent);
             }
         });
-
+        checkPermissions();
+        toogleTracking();
+        updateStatus();
     }
+    private void toogleTracking() {
+
+        if (isTracking) {
+            stopTracking();
+        } else {
+            startTracking();
+        }
+        updateStatus();
+    }
+    private boolean startTracking() {
+        try {
+            if (locationManager == null) {
+                locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+            }
+
+            long minTime = MIN_TIME_BETWEEN_LOCATION_UPDATES;
+            float minDistance = MIN_DISTANCE_MOVED_BETWEEN_LOCATION_UPDATES;
+            Criteria criteria = new Criteria();
+            criteria.setAccuracy(Criteria.ACCURACY_FINE);
+            criteria.setPowerRequirement(Criteria.POWER_MEDIUM);
+
+            if (locationManager != null) {
+                try {
+                    locationManager.requestLocationUpdates(minTime, minDistance, criteria, locationListener, null);
+                    //Use criteria to chose best provider
+                } catch (SecurityException ex) {
+                    // user have disabled location permission - need to validate this permission for newer versions
+                }
+            } else {
+                return false;
+            }
+            isTracking = true;
+            return true;
+        } catch (Exception ex) {
+            //things can go wrong
+            Log.e("TRACKER", "Error during start", ex);
+            return false;
+        }
+    }
+    private boolean stopTracking() {
+        try {
+            try {
+                locationManager.removeUpdates(locationListener);
+                isTracking = false;
+            } catch (SecurityException ex) {
+                //user have disabled location permission - need to validate this permission for newer versions
+            }
+            return true;
+        } catch (Exception ex) {
+            //things can go wrong here as well (listener is null)
+            Log.e("TRACKER", "Error during stop", ex);
+            return false;
+        }
+    }
+
+    private void updateStatus() {
+        if (userLocation != null) {
+            latitude = userLocation.getLatitude();
+            longitude = userLocation.getLongitude();
+        }
+
+        if (userLocation == null && locationManager != null) {
+            //permission check - even though we do get this permission when the app starts up
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                //do nothing
+                Toast.makeText(this, "Need permission for location", Toast.LENGTH_SHORT).show();
+            } else {
+                Location lastGps = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                Location lastNetwork = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            }
+        }
+    }
+    private void broadcastLocationUpdate(Location location){
+        Intent update = new Intent("LOCATION_UPDATE");
+        update.putExtra(EXTRA_USER_LATITUDE, location.getLatitude());
+        update.putExtra(EXTRA_USER_LONGITUDE, location.getLongitude());
+        LocalBroadcastManager.getInstance(this).sendBroadcast(update);
+    }
+    private LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            userLocation = location;
+            updateStatus();
+            broadcastLocationUpdate(location);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+    private void checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // No explanation needed, we can request the permission.
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_LOCATION);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // got permission
+                } else {
+                    // permission denied
+                    //in this case we just close the app
+                    Toast.makeText(this, "You need to enable permission for Location to use the app", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                return;
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
 }
--- app/src/main/java/com/example/st7bac_2020e105/Activities/MapsActivity.java
+++ app/src/main/java/com/example/st7bac_2020e105/Activities/MapsActivity.java
@@ -1,16 +1,144 @@
 package com.example.st7bac_2020e105.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import android.Manifest;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.widget.Toast;
 
 import com.example.st7bac_2020e105.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
-public class MapsActivity extends AppCompatActivity {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+    private double userLatitude;
+    private double userLongitude;
+    private boolean userLocationKnown = false;
+    private boolean tracing = true;
+
+    // ArrayList<VehicleLocation> vehicleLocations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.activity_location);
+
+        Intent data = getIntent();
+        if(data.hasExtra(MainActivity.EXTRA_USER_LONGITUDE) && data.hasExtra(MainActivity.EXTRA_USER_LATITUDE)){
+            userLatitude = data.getDoubleExtra(MainActivity.EXTRA_USER_LATITUDE, 0);
+            userLongitude = data.getDoubleExtra(MainActivity.EXTRA_USER_LONGITUDE, 0);
+            if(userLatitude!=0 && userLongitude!=0) {
+                userLocationKnown = true;
+            }
+        }
+        setUpMapIfNeeded();
     }
+
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMapAsync(this);  //this is the new way
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        // Check if we were successful in obtaining the map.
+        if (mMap != null) {
+            setUpMap();
+            zoomToUser();
+        }
+    }
+
+    private void setUpMap() {
+        if(userLocationKnown) {
+            //Clear map from old markers
+            mMap.clear();
+
+            //Create marker
+            MarkerOptions marker = new MarkerOptions().position(new LatLng(userLatitude, userLongitude)).title("You are here").icon(BitmapDescriptorFactory.fromResource(R.drawable.user_car));
+            //marker.icon(BitmapDescriptorFactory.fromResource(R.drawable.user_car));
+            mMap.addMarker(marker);
+            //mMap.addMarker(new MarkerOptions().position(new LatLng(userLatitude, userLongitude)).title("You are here!"));
+            //new MarkerOptions().icon(BitmapDescriptorFactory.fromResource(R.drawable.car));
+
+            LatLng latlng = new LatLng(userLatitude,userLongitude);
+            //Add circle setup 500m
+            CircleOptions myCircle = new CircleOptions()
+                    .center(latlng)
+                    .radius(500);
+            //plot in google maps - https://developers.google.com/android/reference/com/google/android/gms/maps/model/Circle
+            Circle circle = mMap.addCircle(myCircle);
+            circle.setStrokeColor(Color.RED);
+            circle.setFillColor(0x220000FF);
+            zoomToUser();
+        }
+    }
+    private void zoomToUser(){
+        if(userLocationKnown) {
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                    new LatLng(userLatitude, userLongitude), 15));
+        } else {
+            Toast.makeText(getApplicationContext(), "User location unknown", Toast.LENGTH_SHORT).show();
+        }
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        LocalBroadcastManager.getInstance(this).registerReceiver(locationUpdateReceiver, new IntentFilter("LOCATION_UPDATE"));
+        setUpMapIfNeeded();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(locationUpdateReceiver);
+    }
+
+    BroadcastReceiver locationUpdateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent data) {
+            //Toast.makeText(getApplicationContext(), "Got location update", Toast.LENGTH_SHORT).show();
+            if(data.hasExtra(MainActivity.EXTRA_USER_LONGITUDE) && data.hasExtra(MainActivity.EXTRA_USER_LATITUDE)){
+
+                userLatitude = data.getDoubleExtra(MainActivity.EXTRA_USER_LATITUDE, 0);
+                userLongitude = data.getDoubleExtra(MainActivity.EXTRA_USER_LONGITUDE, 0);
+                if(userLatitude!=0 && userLongitude!=0) {
+                    userLocationKnown = true;
+                }
+            }
+            setUpMap();
+        }
+    };
 }
